5d624f250dc9a0c31db80ca4faadf9a97acb04fa
DevHyung <khuphj@gmail.com>
2018-07-30 01:43:06 +0900

    엑셀저장까지

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 import json
-
+from openpyxl import Workbook
+import os
+#===    GLOBAL
 excelHeader = ['코인명','현재가','전일대비','거래대금']
+FILENAME = 'coin.xlsx'
 def get_symbol_list():
     KRW_File = open("KRW_LIST.txt",'w')
     BTC_File = open("BTC_LIST.txt",'w')
@@ -25,15 +28,28 @@ def get_symbol_list():
     ETH_File.close()
     USDT_File.close()
 def get_KRW_ticker():
+    sheet1.append(excelHeader)
+
     url = "https://api.upbit.com/v1/ticker"
     KRW_File = open("KRW_LIST.txt", 'r')
     lines = KRW_File.readlines()
-    print(lines)
     for line in lines:
         querystring = {"markets": line.strip()}
         response = requests.request("GET", url, params=querystring)
         jsonStr = json.loads(response.text)
-        print(line.strip(),jsonStr[0]['trade_price'],round(jsonStr[0]['acc_trade_price_24h']),round(jsonStr[0]['signed_change_rate']*100,2))
+        sheet1.append([line.strip(),jsonStr[0]['trade_price'],round(jsonStr[0]['signed_change_rate']*100,2),round(jsonStr[0]['acc_trade_price_24h'])])
 if __name__ == "__main__":
-    #get_symbol_list()
+    book = Workbook()
+    # 시트 설정
+    sheet1 = book.active
+    sheet1.column_dimensions['A'].width = 20
+    sheet1.column_dimensions['B'].width = 20
+    sheet1.column_dimensions['C'].width = 20
+    sheet1.column_dimensions['D'].width = 20
+    sheet1.title = 'KRW'
+
+    # 코인파싱
     get_KRW_ticker()
+
+    #
+    book.save(FILENAME)
